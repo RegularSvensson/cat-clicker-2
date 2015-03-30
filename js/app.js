9048f7c3bbd6a0fa0f75de12c788a29758bb23ab
RegularSvensson <elias.svensson.1992@gmail.com>
2015-03-30 21:39:01 +0200

    Add count increment scripts to all cats

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
 
 $('.cat').hide();
 $('#cat1').show();
+
+$( "#catImg1" ).click(function() {
 	var count = $('#count1').text();
 	count++;
 	$('#count1').text(count);
 });
 
-$( "#cat2" ).click(function() {
+$( "#catImg2" ).click(function() {
 	var count = $('#count2').text();
 	count++;
 	$('#count2').text(count);
 });
+
+$( "#catImg3" ).click(function() {
+	var count = $('#count3').text();
+	count++;
+	$('#count3').text(count);
+});
+
+$( "#catImg4" ).click(function() {
+	var count = $('#count4').text();
+	count++;
+	$('#count4').text(count);
+});
+
+$( "#catImg5" ).click(function() {
+	var count = $('#count5').text();
+	count++;
+	$('#count5').text(count);
+});
+
