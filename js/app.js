ba943a0e312e2059f45df43fabd1784ab3a7202b
RegularSvensson <elias.svensson.1992@gmail.com>
2015-03-31 12:23:47 +0200

    Add array of cat objects that store click count, name and imgSrc

--- js/app.js
+++ js/app.js
@@ -2,5 +2,32 @@
 /* ======= Model ======= */
 var model = {
 	currentCat: null,
+	cats: [
+		{
+			clickcount: 0,
+			name: 'Meron',
+			imgSrc: 'img/cat1.jpg'
+		},
+		{
+			clickcount: 0,
+			name: 'Paula',
+			imgSrc: 'img/cat2.jpg'
+		},
+		{
+			clickcount: 0,
+			name: 'Isak',
+			imgSrc: 'img/cat3.jpg'
+		},
+		{
+			clickcount: 0,
+			name: 'Robban',
+			imgSrc: 'img/cat4.jpg'
+		},
+		{
+			clickcount: 0,
+			name: 'Tim',
+			imgSrc: 'img/cat5.jpg'
+		}
+	]
 }
 
