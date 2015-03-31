b3b8fdbec3cfd8330abc7630efefa47ce096a680
RegularSvensson <elias.svensson.1992@gmail.com>
2015-03-31 14:38:38 +0200

    Trial and big error... Roll back to burn-stage

--- js/app.js
+++ js/app.js
@@ -1,33 +1,67 @@
 
 /* ======= Model ======= */
 var model = {
-	currentCat: null,
-	cats: [
-		{
-			clickcount: 0,
-			name: 'Meron',
-			imgSrc: 'img/cat1.jpg'
-		},
-		{
-			clickcount: 0,
-			name: 'Paula',
-			imgSrc: 'img/cat2.jpg'
-		},
-		{
-			clickcount: 0,
-			name: 'Isak',
-			imgSrc: 'img/cat3.jpg'
-		},
-		{
-			clickcount: 0,
-			name: 'Robban',
-			imgSrc: 'img/cat4.jpg'
-		},
-		{
-			clickcount: 0,
-			name: 'Tim',
-			imgSrc: 'img/cat5.jpg'
-		}
-	]
-}
+    currentCat: null,
+    cats: [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/cat1.jpg'
+        }
+    ]
+};
 
+/* ======= Octopus ======= */
+
+var octopus = {
+
+    init: function() {
+        // set our current cat to the first one in the list
+        model.currentCat = model.cats[0];
+
+        // tell our views to initialize
+        //catListView.init();
+        catView.init();
+    },
+
+    getCurrentCat: function() {
+        return model.currentCat;
+    },
+
+    getCats: function() {
+        return model.cats;
+    },
+
+    // set the currently-selected cat to the object passed in
+    setCurrentCat: function(cat) {
+        model.currentCat = cat;
+    },
+
+    // increments the counter for the currently-selected cat
+};
+
+/* ======= View ======= */
+
+var catView = {
+
+    init: function() {
+        // store pointers to our DOM elements for easy access later
+        this.catElem = document.getElementById('cat');
+        this.catNameElem = document.getElementById('cat-name');
+        this.catImageElem = document.getElementById('cat-img');
+        this.countElem = document.getElementById('cat-count');
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
+    render: function() {
+        // update the DOM elements with values from the current cat
+        var currentCat = octopus.getCurrentCat();
+        this.countElem.textContent = currentCat.clickCount;
+        this.catNameElem.textContent = currentCat.name;
+        this.catImageElem.src = currentCat.imgSrc;
+    }
+};
+
+octopus.init();
